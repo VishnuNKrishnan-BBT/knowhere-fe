5ecbb932745a015307155c896330ed43110e0d09
VishnuNKrishnan <navaneeth.vishnu1@gmail.com>
2023-11-12 02:14:50 +0400

    Endpoint update

--- src/supportFunctions/getAllVehicleList.js
+++ src/supportFunctions/getAllVehicleList.js
@@ -10,8 +10,8 @@ export const getAllVehiclesList = async () => {
     try {
         // Handle the response data as needed
         // logger('log', (`getAllVehiclesList(1212621 Hardcoded)`, response.data))
-        // const response = await axios.get(`${process.env.REACT_APP_KW_MS_VEHICLEDATA_BE_HOST}/allVehicles`, { headers })
-        const response = await axios.get(`http://127.0.0.1:4000/allVehicles`, { headers })
+        const response = await axios.get(`${process.env.REACT_APP_KW_MS_VEHICLEDATA_BE_HOST}/allVehicles`, { headers })
+        // const response = await axios.get(`http://127.0.0.1:4000/allVehicles`, { headers })
         logger('log', response?.data.data)
         return response.data.data
     } catch (error) {
