ca7e2cb4cb6216d1752315262e017f6cc5b6c25d
VishnuNKrishnan <navaneeth.vishnu1@gmail.com>
2024-01-05 21:23:26 +0400

    Export statement corrected

--- src/mapUtils/mapManipulators.js
+++ src/mapUtils/mapManipulators.js
@@ -1,58 +1,48 @@
-const doInitalFly = (mapRef = null, lat = null, lon = null) => {
+export const doInitalFly = (mapRef = null, lat = null, lon = null) => {
     if (mapRef == null || lat == null || lon == null) {
         return
     }
     mapRef.current.flyTo({ center: [lon, lat], duration: 1000, zoom: 12 })
 }
 
-const flyToSite = (mapRef = null, lat = null, lon = null, viewPortParams = {}, duration = 2000) => {
+export const flyToSite = (mapRef = null, lat = null, lon = null, viewPortParams = {}, duration = 2000) => {
     if (mapRef == null || lat == null || lon == null) {
         return
     }
     mapRef.current.flyTo({ center: [lon, lat], duration: duration, ...viewPortParams })
 }
 
-const recenter = (mapRef = null, lat = null, lon = null) => {
+export const recenter = (mapRef = null, lat = null, lon = null) => {
     if (mapRef == null || lat == null || lon == null) {
         return
     }
     mapRef.current.flyTo({ center: [lon, lat], duration: 1000 })
 }
 
-const zoomIn = (mapRef = null, viewport = {}) => {
+export const zoomIn = (mapRef = null, viewport = {}) => {
     if (mapRef == null) {
         return
     }
     mapRef.current.flyTo({ zoom: viewport.zoom + 1, duration: 250 })
 }
 
-const zoomOut = (mapRef = null, viewport = {}) => {
+export const zoomOut = (mapRef = null, viewport = {}) => {
     if (mapRef == null) {
         return
     }
     mapRef.current.flyTo({ zoom: viewport.zoom - 1, duration: 250 })
 }
 
-const rotateLeft = (mapRef = null, viewport = {}) => {
+export const rotateLeft = (mapRef = null, viewport = {}) => {
     if (mapRef == null) {
         return
     }
     mapRef.current.flyTo({ bearing: viewport.bearing + 22.5, duration: 250 })
 }
 
-const rotateRight = (mapRef = null, viewport = {}) => {
+export const rotateRight = (mapRef = null, viewport = {}) => {
     if (mapRef == null) {
         return
     }
     mapRef.current.flyTo({ bearing: viewport.bearing - 22.5, duration: 250 })
-}
-
-module.exports = {
-    doInitalFly,
-    flyToSite,
-    recenter,
-    zoomIn,
-    zoomOut,
-    rotateLeft,
-    rotateRight
 }
